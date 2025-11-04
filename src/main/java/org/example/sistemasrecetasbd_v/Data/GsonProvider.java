f56a6443dcff778044edf41c633e2a55d90e8bc1
Julian <lanrarias@gmail,com>
2025-11-04 17:32:47 -0600

    Cambio en GsonProvider

--- src/main/java/org/example/sistemasrecetasbd_v/Data/GsonProvider.java
+++ src/main/java/org/example/sistemasrecetasbd_v/Data/GsonProvider.java
@@ -2,17 +2,13 @@ package org.example.sistemasrecetasbd_v.Data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.time.LocalDate;
 
 public final class GsonProvider {
     private static final Gson GSON = new GsonBuilder()
             .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-            .serializeNulls()
             .setPrettyPrinting()
             .create();
-
-    private GsonProvider() {}
 
     public static Gson get() { return GSON; }
 }
